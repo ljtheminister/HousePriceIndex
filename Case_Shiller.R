261c5bbcfdb7ea5a6796d4fc58f2fb188b22cf32
ljtheminister <john.c.min@gmail.com>
2014-10-08 22:28:24 -0400

    finishing up

--- Case_Shiller.R
+++ Case_Shiller.R
@@ -11,32 +11,25 @@
 #   Comment and structure your code to be easily followed.
 #
 library(McSpatial)
+library(dplyr)
+
+# Read data
 setwd('/home/lj/ML/Zillow/')
 data <- read.csv("Transactions.csv")
 
+# Data Pre-processing
 data$transdate <- as.Date(data$transdate, format="%m/%d/%Y")
 data$transdate_previous <- as.Date(data$transdate_previous, format="%m/%d/%Y")
-data$gaptime <- data$transdate - data$transdate_previous
-
 data <- data[!is.na(data$transdate_previous),]
 
-
-names(data)
-repsales <- data[duplicated(data$propertyid),]
-
-
-min_date <- as.Date(min(transdate_previous, na.rm=TRUE))
-max_date <- as.Date(max(transdate, na.rm=TRUE))
-
 # sequence of dates in date_range
+min_date <- as.Date(min(data$transdate_previous, na.rm=TRUE))
+max_date <- as.Date(max(data$transdate, na.rm=TRUE))
 date_range <- as.character(seq(from=min_date, to=max_date, by=1))
 
-date_map = list()
-for (i in 1:length(date_range)) {
-  date_map[date_range[i]] <- i
-}
+# Fix chain of transvalue, transdates
+propertyid_groups <- split(data, data$propertyid) # split/subset data by propertyid
 
-propertyid_groups <- split(data, data$propertyid)
 fix_chain <- function(grp) {
   for (i in (1:nrow(grp)-1)) {
     grp$transvalue[i] <- grp$transvalue_previous[i+1]
@@ -45,5 +38,37 @@ fix_chain <- function(grp) {
   return (grp)
 }
 
-lapply(s, fix_chain)
-unsplit(t, .(propertyid))
+propertyid_groups <- lapply(propertyid_groups, fix_chain) # apply fix_chain function to each subset of data by propertyid
+data <- unsplit(propertyid_groups, data$propertyid) # rbind list back into dataframe
+data$gaptime <- data$transdate - data$transdate_previous # compute gaptime
+
+#write.csv(data, file='data.csv')
+
+## Time Intervals/Periods for Repeat Sales Method
+date_map = list()
+daily_periods <- function(date_map) {
+  for (i in 1:length(date_range)) {
+    date_map[date_range[i]] <- i-1
+  }
+  return (date_map)
+}
+
+process_daily_periods <- function(data, date_map) {
+  for (i in 1:nrow(data)) {
+    row <- data[i,]
+    data[i, 't1'] <- date_map[as.character(row$transdate)]
+    data[i, 't0'] <- date_map[as.character(row$transdate_previous)]
+  }
+}
+
+# annual time periods
+data$t0 <- year(data$transdate_previous) - 1988
+data$t1 <- year(data$transdate) - 1988
+
+# Repeat Sales Method using McSpatial Library
+fit_BMN_abs <- repsale(price0=data$transvalue_previous, price1=data$transvalue, time0=data$t0, time1=data$t1, stage3="abs")
+fit_BMN_sq <- repsale(price0=data$transvalue_previous, price1=data$transvalue, time0=data$t0, time1=data$t1, stage3="square")
+
+# Let's build our own Case-Shiller Repeat Sales Model
+
+

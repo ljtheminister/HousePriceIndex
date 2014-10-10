41d11a52ea854c526c52e8949246c52a48e63ee9
ljtheminister <john.c.min@gmail.com>
2014-10-09 22:17:09 -0400

    C-S model uses estimated std deviations, not variances for weights

--- Case_Shiller.R
+++ Case_Shiller.R
@@ -15,19 +15,13 @@ library(dplyr)
 library(lubridate)
 
 # Read data
-setwd('/Users/lj/Downloads/')
+setwd('/home/lj/ML/Zillow/') # set working directory
 data <- read.csv("Transactions.csv")
 
 # Data Pre-processing
 data$transdate <- as.Date(data$transdate, format="%m/%d/%Y")
 data$transdate_previous <- as.Date(data$transdate_previous, format="%m/%d/%Y")
-data <- data[!(is.na(data$transdate_previous)),] # drop observations with NA transdate_previous because same observations as NA transvalue_previous
-
-# sequence of dates in date_range to convert dates to ints as time periods
-min_date <- as.Date(min(data$transdate_previous, na.rm=TRUE))
-max_date <- as.Date(max(data$transdate, na.rm=TRUE))
-min_year <- year(min_date)
-date_range <- as.character(seq(from=min_date, to=max_date, by=1))
+data <- data[!(is.na(data$transdate_previous)),] # remove single sales from data set
 
 ## Fix chain of transvalue, transdates
 propertyid_groups <- split(data, data$propertyid) # split/subset data by propertyid
@@ -46,31 +40,24 @@ data <- unsplit(propertyid_groups, data$propertyid) # rbind list back into dataf
 
 
 ## Time Intervals/Periods for Repeat Sales Method
-date_map = list()
-daily_periods <- function(date_map) {
-  for (i in 1:length(date_range)) {
-    date_map[date_range[i]] <- i-1
-  }
-  return (date_map)
-}
-
-process_daily_periods <- function(data, date_map) {
-  for (i in 1:nrow(data)) {
-    row <- data[i,]
-    data[i, 't1'] <- date_map[as.character(row$transdate)]
-    data[i, 't0'] <- date_map[as.character(row$transdate_previous)]
-  }
-}
+# sequence of dates in date_range to convert dates to ints as time periods
+# we don't use this daily chunk of code
+min_date <- as.Date(min(data$transdate_previous, na.rm=TRUE))
+max_date <- as.Date(max(data$transdate, na.rm=TRUE))
+min_year <- year(min_date)
 
 # annual time periods
 data$t0 <- year(data$transdate_previous) - min_year + 1
 data$t1 <- year(data$transdate) - min_year + 1
 
+data$p1 <- log(data$transvalue)
+data$p0 <- log(data$transvalue_previous)
+
 # Repeat Sales Method using McSpatial Library
-fit_CS_McSpatial <- repsale(price0=data$transvalue_previous, price1=data$transvalue, time0=data$t0, time1=data$t1, mergefirst=0, stage3="square")
+fit_CS_McSpatial <- repsale(price0=data$p0, price1=data$p1, time0=data$t0, time1=data$t1, mergefirst=0, stage3="square")
 
 ## Let's build our own Case-Shiller Repeat Sales Model
-dy <- data$transvalue - data$transvalue_previous
+dy <- data$p1 - data$p0
 times <- levels(factor(c(data$t0, data$t1)))
 
 K = length(times)
@@ -84,12 +71,35 @@ for (j in seq(1, K)) {
 }
 
 # Step 1: Fit BMN model
-fit1 <- lm(dy ~ X) # no intercept
+fit1 <- lm(dy ~ X + 0) # regression with no intercept
 e <- residuals(fit1) # take the residuals
-data$saletime <- data$t1 - data$t0 
+data$saletime <- data$t1 - data$t0 # compute interval time between sales
 # Step 2: Fit residuals
-fit2 <- lm(e^2 ~ data$saletime) # fit the residuals
+fit2 <- lm(e^2 ~ data$saletime) # regress sqiared residuals against gap time
 W <- fitted(fit2)
-W <- ifelse(W>0, 1/sqrt(W), 0) # make non-negative, take reciprocal of sqrt of fitted values
+W <- ifelse(W>0, 1/sqrt(W), 0) # make non-negative, take reciprocal of sqrt of fitted values - estimated standard deviations used
 # Step 3: Weighted Least Squares regression
 fit_CS <- lm(dy ~ X + 0, weights=W)
+
+
+## Code I don't use 
+if(FALSE) {
+  date_range <- as.character(seq(from=min_date, to=max_date, by=1))
+  
+  date_map = list()
+  daily_periods <- function(date_map) {
+    for (i in 1:length(date_range)) {
+      date_map[date_range[i]] <- i-1
+    }
+    return (date_map)
+  }
+  
+  process_daily_periods <- function(data, date_map) {
+    for (i in 1:nrow(data)) {
+      row <- data[i,]
+      data[i, 't1'] <- date_map[as.character(row$transdate)]
+      data[i, 't0'] <- date_map[as.character(row$transdate_previous)]
+    }
+  }
+  
+}
